f44bd438b632653b2df78ccb8760df2d73bf4849
Joseph Bangoura <ptijjo@hotmail.com>
2024-01-08 12:53:36 +0100

    images

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 const Products: React.FC = () => {
     return (
         <div className='products'>
-            produits
+            <Carousel>
+                <div>
+                    <img src="src/assets/picture/IMG-20240108-WA0001.jpg" alt="" />
+                    <p>Legend1</p>
+                </div>
+                <div>
+                    <img src="src/assets/picture/IMG-20240108-WA0002.jpg" alt="" />
+                    <p>Legend2</p>
+                </div>
+                <div>
+                    <img src="src/assets/picture/IMG-20240108-WA0003.jpg" alt="" />
+                    <p>Legend3</p>
+                </div>
+            </Carousel>
         </div>
     )
-}
+};
 
 export default Products
