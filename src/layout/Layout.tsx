77327c975eaa6f21f0b98b4eb517f6ed823561b9
Joseph Bangoura <ptijjo@hotmail.com>
2024-01-08 10:26:48 +0100

    page resultat

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Header from '../components/Header'
 import Presentation from '../pages/Presentation'
 import Products from '../pages/Products'
+import Resultats from '../pages/Resultats'
 
 const Layout : React.FC= () => {
   return (
     <>
       <Header />
       <Presentation />
-      <Products/>
+      <Products />
+      <Resultats/>
           
     </>
   )
---
+++ src/pages/Resultats.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Resultats : React.FC= () => {
+  return (
+    <div className='resultats'>
+      avant/apres
+    </div>
+  )
+}
+
+export default Resultats
